bab511aebf2bc7f70ff424c84f6048c1832e1c10
r.kazakov <r.kazakov@corp.mail.ru>
2021-01-13 17:35:42 +0300

    Task 3. Show timestamp every 1 second.

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+from datetime import datetime
 import asyncio
+
 import gui
 
 
-def main():
-    loop = asyncio.get_event_loop()
+async def generate_messages(messages_queue: asyncio.Queue):
+    while True:
+        msg = f"Ping {datetime.now().timestamp():.0f}"
+        messages_queue.put_nowait(msg)
+        await asyncio.sleep(1)
 
+
+async def main():
     messages_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
 
-    loop.run_until_complete(gui.draw(messages_queue, sending_queue, status_updates_queue))
+    await asyncio.gather(gui.draw(messages_queue, sending_queue, status_updates_queue),
+                         generate_messages(messages_queue))
 
 
 if __name__ == '__main__':
-    main()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
