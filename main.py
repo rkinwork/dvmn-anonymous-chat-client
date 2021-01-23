9c4a51deadc5c3985ceef1fb7b8d6f975ca7f378
rkinwork <rkinwork@gmail.com>
2021-01-23 21:41:24 +0300

    Task 7. Send chat message like debug message to console

--- main.py
+++ main.py
@@ -21,6 +21,12 @@ async def read_msgs(host: str, port: str, message_queue: asyncio.Queue, save_mes
         await asyncio.sleep(ATTEMPT_DELAY_SECS)
 
 
+async def send_msgs(host: str, port: str, sending_queue: asyncio.Queue):
+    while True:
+        msg = await sending_queue.get()
+        print(f'Пользователь написал: {msg}')
+
+
 async def save_messages(filepath, queue: asyncio.Queue):
     async with aiofiles.open(filepath, 'a', buffering=1) as log_file:
         while True:
@@ -28,7 +34,7 @@ async def save_messages(filepath, queue: asyncio.Queue):
             await log_file.write(f'{message_to_save}\n')
 
 
-def load_chat_history(filepath, queue: asyncio.Queue):
+def load_chat_history(filepath, queue: asyncio.Queue):  # gui blocking risk
     with open(filepath) as log_file:
         for row in log_file:
             queue.put_nowait(row.strip())
@@ -42,10 +48,10 @@ async def main():
     save_message_queue = asyncio.Queue()
 
     load_chat_history(conf.filepath, messages_queue)
-
     await asyncio.gather(gui.draw(messages_queue, sending_queue, status_updates_queue),
-                         read_msgs(conf.host, conf.port, messages_queue, save_message_queue),
                          save_messages(conf.filepath, save_message_queue),
+                         read_msgs(conf.host, conf.port, messages_queue, save_message_queue),
+                         send_msgs(conf.host, conf.port, sending_queue),
                          )
 
 
