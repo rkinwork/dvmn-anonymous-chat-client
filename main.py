c23d218cbd86115a0d77c9223ba7e5d973896b42
rkinwork <rkinwork@gmail.com>
2021-01-24 19:49:50 +0300

    Task 8. Add authorization to the startup of the program

--- main.py
+++ main.py
@@ -1,43 +1,8 @@
 import asyncio
-
-import aiofiles
+import logging
 
 import gui
-from utils import setup_config, open_connection, ATTEMPT_DELAY_SECS
-
-
-async def read_msgs(host: str, port: str, message_queue: asyncio.Queue, save_message_queue: asyncio.Queue):
-    while True:
-        async with open_connection(host, port) as rw:
-            reader, *_ = rw
-            while True:
-                row = await reader.readline()
-                if not row:
-                    break
-                row = row.decode('utf8').strip()
-                message_queue.put_nowait(row)
-                save_message_queue.put_nowait(row)
-
-        await asyncio.sleep(ATTEMPT_DELAY_SECS)
-
-
-async def send_msgs(host: str, port: str, sending_queue: asyncio.Queue):
-    while True:
-        msg = await sending_queue.get()
-        print(f'Пользователь написал: {msg}')
-
-
-async def save_messages(filepath, queue: asyncio.Queue):
-    async with aiofiles.open(filepath, 'a', buffering=1) as log_file:
-        while True:
-            message_to_save = await queue.get()
-            await log_file.write(f'{message_to_save}\n')
-
-
-def load_chat_history(filepath, queue: asyncio.Queue):  # gui blocking risk
-    with open(filepath) as log_file:
-        for row in log_file:
-            queue.put_nowait(row.strip())
+from utils import setup_config, load_chat_history, save_messages, read_msgs, send_msgs
 
 
 async def main():
@@ -47,11 +12,14 @@ async def main():
     status_updates_queue = asyncio.Queue()
     save_message_queue = asyncio.Queue()
 
+    if conf.debug:
+        logging.basicConfig(level=logging.DEBUG)
+
     load_chat_history(conf.filepath, messages_queue)
     await asyncio.gather(gui.draw(messages_queue, sending_queue, status_updates_queue),
                          save_messages(conf.filepath, save_message_queue),
-                         read_msgs(conf.host, conf.port, messages_queue, save_message_queue),
-                         send_msgs(conf.host, conf.port, sending_queue),
+                         read_msgs(conf.host, conf.lport, messages_queue, save_message_queue),
+                         send_msgs(conf.host, conf.port, conf.token, sending_queue),
                          )
 
 
--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 import sys
 import contextlib
-import datetime
+import json
+import logging
 
 import asyncio
 import configargparse
 from aiofiles.os import wrap as aiowrap
+import aiofiles
 
 DEFAULT_LOG_DESCRIPTOR = aiowrap(sys.stdout.write)
 LOG_DTTM_TMPL = '%d-%m-%y %H:%M:%S'
@@ -15,12 +17,17 @@ DEFAULT_LISTEN_SERVER_PORT = 5000
 DEFAULT_SEND_SERVER_PORT = 5050
 DEFAULT_HISTORY_FILE = 'minechat.history'
 
+a_log_debug = aiowrap(logging.debug)
+a_log_error = aiowrap(logging.error)
+
 
 def setup_config():
     p = configargparse.ArgParser(default_config_files=['conf.ini'])
     p.add_argument('-u', '--host', help='host of the chat server', env_var='DVMN_HOST', default=DEFAULT_SERVER_HOST)
-    p.add_argument('-p', '--port', help='port of the chat server', env_var='DVMN_SEND_PORT',
+    p.add_argument('-p', '--port', help='transmit port of the chat server', env_var='DVMN_SEND_PORT',
                    default=DEFAULT_SEND_SERVER_PORT)
+    p.add_argument('-l', '--lport', help='listen port of the chat server', env_var='DVMN_PORT',
+                   default=DEFAULT_LISTEN_SERVER_PORT)
     p.add_argument('-d', '--debug', help='switch on debug mode', env_var='DVMN_DEBUG', action='store_true')
     p.add_argument('-f', '--filepath', help='path to the file where to store chat history',
                    env_var='DVMN_CHAT_PATH', default=DEFAULT_HISTORY_FILE)
@@ -31,8 +38,64 @@ def setup_config():
     return p.parse_args()
 
 
+async def authorise(token, reader, writer):
+    await a_log_debug(decode_message(await reader.readline()))
+    writer.write(f"{token}\n".encode())
+    response = json.loads(await reader.readline())
+    if not response:
+        await a_log_error("Unknown token. Check it, or register new user")
+        return False
+    await a_log_debug(response)
+    await a_log_debug(f"Выполнена авторизация. Пользователь {response.get('nickname', 'None')}.")
+    return True
+
+
 def decode_message(message):
     return message.decode('utf-8').strip()
+
+
+async def read_msgs(host: str, port: str, message_queue: asyncio.Queue, save_message_queue: asyncio.Queue):
+    while True:
+        async with open_connection(host, port) as rw:
+            reader, *_ = rw
+            while True:
+                row = await reader.readline()
+                if not row:
+                    break
+                row = row.decode('utf8').strip()
+                message_queue.put_nowait(row)
+                save_message_queue.put_nowait(row)
+
+        await asyncio.sleep(ATTEMPT_DELAY_SECS)
+
+
+async def send_msgs(host: str, port: str, token: str, sending_queue: asyncio.Queue):
+    async with open_connection(host, port) as rw:
+        reader, writer = rw
+        if await authorise(token, reader, writer):
+            auth_response = decode_message(await reader.readline())
+            await a_log_debug(auth_response)
+        else:
+            await a_log_error('Problems with authorization. Check your token')
+            # TODO надо завершать исполнение программы если авторизация не прошла в начале
+            return
+
+        while True:
+            msg = await sending_queue.get()
+            await a_log_debug(f'Пользователь написал: {msg}')
+
+
+async def save_messages(filepath, queue: asyncio.Queue):
+    async with aiofiles.open(filepath, 'a', buffering=1) as log_file:
+        while True:
+            message_to_save = await queue.get()
+            await log_file.write(f'{message_to_save}\n')
+
+
+def load_chat_history(filepath, queue: asyncio.Queue):  # gui blocking risk
+    with open(filepath) as log_file:
+        for row in log_file:
+            queue.put_nowait(row.strip())
 
 
 async def dummy_message_writer(msg):
